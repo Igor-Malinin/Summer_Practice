ebe5924414504443ba1ede9f5aed74730e4f8d76
Igor-Malinin <86961903+Igor-Malinin@users.noreply.github.com>
2021-07-05 18:06:52 +0600

    Add files via upload

---
+++ spring_storage/src/test/java/com/storage_practice/spring_storage/SpringStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage_practice.spring_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
