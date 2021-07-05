9f74b6da7ad3a5a357e9ba36626d9558ded259f3
Igor-Malinin <86961903+Igor-Malinin@users.noreply.github.com>
2021-07-05 18:02:09 +0600

    Add files via upload

---
+++ Spring/src/main/java/com/storage_practice/demo/StorageApplication.java
@@ -0,0 +1,13 @@
+package com.storage_practice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageApplication.class, args);
+	}
+
+}
